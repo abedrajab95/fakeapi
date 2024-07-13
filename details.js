b312529cea8661e13906f295b6687c9aac69d399
Abdulfattah Rajab <abedrajab95@outlook.com>
2024-07-13 22:20:04 +0300

    first commit

---
+++ details.js
@@ -0,0 +1,22 @@
+const getProducts = async()=>{
+
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get('id');
+    const {data} = await axios.get (`http://fakestoreapi.com/products/${id}`)
+    return data;
+
+}
+
+const displayProduct = async ()=>{
+    const data = await getProducts();
+    const result = `
+    <h2>${data.title}</h2>
+    <p>${data.price}</p>
+    <img src='${data.image}' />
+    
+    `;
+
+    document.querySelector(".product").innerHTML=result;
+}
+
+displayProduct();
---
+++ main.js
@@ -0,0 +1,18 @@
+const getProducts = async () => {
+    const { data } = await axios.get(`http://fakestoreapi.com/products`);
+    const result = data
+      .map((product) => {
+        return `
+              <div class="product">
+              <h2>${product.title}</h2>
+              <img src='${product.image}'/>
+              <a href='details.html?id=${product.id}'>Details</a>
+              </div>
+          `;
+      })
+      .join("");
+    document.querySelector(".products").innerHTML = result;
+  };
+  getProducts();
+
+
